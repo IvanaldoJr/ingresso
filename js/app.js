ec3e429655212a62bd8aae734ac752cbae76e5ca
Ivanaldo Junior <ivanaldochagas10@gmail.com>
2024-03-06 20:58:15 -0300

    alteração app

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@ function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
     let quantidade   = document.getElementById('qtd');
     alert(tipo.value);
-    alert(quantidade.value);
+    
 }
