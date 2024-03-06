788b5fec7a4cfb0eb5fa90e5d6e1b7c7f2f3a053
Ivanaldo Junior <juniorchagas@gazetaweb.com>
2024-03-06 16:30:57 -0300

    recuperando os valores do html

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function comprar() {
+    let tipo = document.getElementById('tipo-ingresso');
+    let quantidade   = document.getElementById('qtd');
+    alert(tipo.value);
+    alert(quantidade.value);
+}
